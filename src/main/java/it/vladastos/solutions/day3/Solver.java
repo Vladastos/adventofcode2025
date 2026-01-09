dcf5ab7299f3a7d18792ba4cb97ea73bfcdcc9cd
Vladislav Parfeniuc <vladislav.parfeniuc@logobject.ch>
2026-01-09 09:22:07 +0100

    wip day3

---
+++ src/main/java/it/vladastos/solutions/day3/Solver.java
@@ -0,0 +1,11 @@
+package it.vladastos.solutions.day3;
+
+import it.vladastos.AbstractSolver;
+
+public class Solver extends AbstractSolver {
+    
+    @Override
+    public String solvePart1() {
+        return "your code goes here";
+    }
+}
